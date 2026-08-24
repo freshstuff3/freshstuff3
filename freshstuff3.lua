4c213e65709797fc6fc6879811936992e39f9ce1
pathcheatsheet <szakal.gergo@gmail.com>
2026-08-24 19:02:35 +0200

    more package.path loading fixes for PX

--- freshstuff3.lua
+++ freshstuff3.lua
@@ -1,5 +1,24 @@
 --- freshstuff3.lua
 --- Entry point for freshstuff3
 ---@todo NMDC
-require "host.ptokax"
-if type(Core) ~= "table" then OnStartup() end
+
+if type(Core) == "table" and type(Core.GetPtokaXPath) == "function" then
+    local path_separator = package.config:sub(1, 1)
+    local ptokax_path = Core.GetPtokaXPath()
+    assert(type(ptokax_path) == "string", "Core.GetPtokaXPath() must return a string")
+    if not ptokax_path:match("[/\\]$") then
+        ptokax_path = ptokax_path .. path_separator
+    end
+
+    local host_path = ptokax_path .. "scripts" .. path_separator ..
+        "freshstuff3" .. path_separator .. "host" .. path_separator .. "ptokax.lua"
+    package.loaded["host.ptokax"] = dofile(host_path) or true
+else
+    if package.config:sub(1, 1) == "\\" then
+        package.path = package.path .. ";C:\\freshstuff3\\freshstuff3\\?.lua"
+    else
+        package.path = package.path .. ";/freshstuff3/freshstuff3/?.lua"
+    end
+    require "host.ptokax"
+    OnStartup()
+end
